34f978bcc531f72f8777b2024120c524094e5914
Atul Tripathi <atul31815@gmail.com>
2024-01-28 18:46:31 +0530

    Q2

--- String/Q2.js
+++ String/Q2.js
@@ -9,3 +9,48 @@
 // Output: Strings are not rotations of each other
 
 
+let s1 = 'abcad'
+let s2 = 'cadab'
+
+function checkString(s1, s2, ind, Size) {
+    if (ind === Size) {
+        return false
+    }
+    for (let i = 0; i < Size; i++) {
+        if (s1[i] !== s2[(ind + i) % Size]) {
+            return false
+        }
+    }
+    return true
+}
+
+if (s1.length !== s2.length) {
+    console.log('Strings are not rotations of each other')
+} else {
+    let indexes = []
+    let firstLetter = s1[0]
+    let Size = s1.length
+    // Store the indexes of all occurrences of first character of s1 in s2
+    for (let i = 0; i < s2.length; i++) {
+        if (s2[i] === firstLetter) {
+            indexes.push(i)
+        }
+    }
+
+    let isRotation = false
+    // Check if all occurrences of first character are rotations of each other
+    for (let i of indexes) {
+        isRotation = checkString(s1, s2, i, Size)
+        
+        if (isRotation) {
+            break
+        }
+    }
+
+    if (isRotation) {
+        console.log('Strings are rotations of each other')
+    }
+    else {
+        console.log('Strings are not rotations of each other')
+    }
+}
